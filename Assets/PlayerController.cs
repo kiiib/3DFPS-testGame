610c526e0d272daf84273ee4c4fe7901d4cc247b
Jian-Bin Liu <billyljb@gmail.com>
2017-04-11 21:09:24 +0800

    update

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Animator animatorController;
+
+	// Use this for initialization
+	void Start () {
+        animatorController = this.GetComponent<Animator>();
+
+    }
+
+    public float MoveSpeed;
+    float currentSpeed = 0;
+
+	// Update is called once per frame
+	void Update () {
+        float result = 0;
+
+        if (Input.GetKey(KeyCode.W)) {
+            result += MoveSpeed;
+        }
+
+        currentSpeed = result;
+        this.transform.position += Time.deltaTime * currentSpeed * this.transform.forward;
+
+        animatorController.SetFloat("Speed", currentSpeed);
+	}
+}
